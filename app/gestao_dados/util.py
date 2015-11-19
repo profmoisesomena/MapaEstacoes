eee2db44bcd910c9d36de73488b57be0b32ce4dc
LEDS <leds.ifes@gmail.com>
2015-11-18 23:22:04 -0200

    Update util.py

--- app/gestao_dados/util.py
+++ app/gestao_dados/util.py
@@ -27,18 +27,3 @@ class Base(db.Model):
     #date_created  = db.Column(db.DateTime,  default=db.func.current_timestamp())
     #date_modified = db.Column(db.DateTime,  default=db.func.current_timestamp(),
     #                                       onupdate=db.func.current_timestamp())
-
-class endereco():
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sockfd = sock.fileno()
-    SIOCGIFADDR = 0x8915
-
-    def get_ip(iface = 'eth0'):
-        ifreq = struct.pack('16sH14s', iface, socket.AF_INET, '\x00'*14)
-        try:
-            res = fcntl.ioctl(sockfd, SIOCGIFADDR, ifreq)
-        except:
-            return None
-        ip = struct.unpack('16sH2x4s8x', res)[2]
-        return socket.inet_ntoa(ip)
