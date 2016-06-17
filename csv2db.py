935f28ed5a2eb7d7944e2e40044f8fbf4aba5f7c
profmoisesomena <prof.moisesomena@gmail.com>
2016-06-17 09:26:27 -0300

    correction name table

--- csv2db.py
+++ csv2db.py
@@ -5,4 +5,4 @@ from sqlalchemy import create_engine
 dados_estacoes =pd.read_csv("estacoes_es.csv", encoding='latin-1',sep=',')
 engine = create_engine('postgresql://postgres:123456@localhost:5432/sigmap')
 df = pd.DataFrame(dados_estacoes)
-df.to_sql('estacoes_es', engine)
+df.to_sql('estacao_meteorologica', engine)
